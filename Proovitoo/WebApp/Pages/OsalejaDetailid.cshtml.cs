7e93bc2b9ab903774cb114cdd614ab0f15058e6f
kalevtoom <2YPSBw5eCAPe>
2022-01-21 21:48:11 +0200

    natuke kujundust

---
+++ Proovitoo/WebApp/Pages/OsalejaDetailid.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class OsalejaDetailid : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Proovitoo/WebApp/Pages/YrituseLisamine.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class YrituseLisamine : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
